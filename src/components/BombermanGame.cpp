8b658f57009b3ac692ef0c0ae63a42476439d6f6
kadank <kadank3@gmail.com>
2026-06-18 17:54:59 +0200

    Improved pause

--- src/components/BombermanGame.cpp
+++ src/components/BombermanGame.cpp
@@ -64,9 +64,15 @@ void BombermanGame::Tick() {
 }
 
 void BombermanGame::OnDrawTimer(wxTimerEvent& event) {
-    if(pressedKeys.contains('P') && pause_delay <= 0) {
+    if(pressedKeys.contains(27) && pause_delay <= 0) {
         board.SetPause(!board.IsPaused());
-
+        board.showOverlay(wxT("Pauza"), "", wxT("Powrót do menu"), this->board.onMainMenu, wxT("Kontynuuj"),
+                          [this] {
+                              board.SetPause(false);
+                          });
+        if (board.IsPaused()==false) {
+            board.hideOverlay();
+        }
         pause_delay = 20;
     }
     if(!board.IsPaused()) Tick();
